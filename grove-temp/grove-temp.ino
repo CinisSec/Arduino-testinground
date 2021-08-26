d3862e44a0fb8abbf99e675ae486090d40460a21
CinisSec <gregorypro@protonmail.com>
2021-08-26 22:15:37 +0200

    Update grove-temp.ino

--- grove-temp/grove-temp.ino
+++ grove-temp/grove-temp.ino
@@ -1,11 +1,21 @@
 #include <math.h>
 #include "rgb_lcd.h"
 
+#define ROTARY_ANGLE_SENSOR A1
+
+#define ADC_REF 5   //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
+                    //board switches to 3V3, the ADC_REF should be 3.3
+#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
+
+#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
+
 rgb_lcd lcd;
 
-const int colorR = 255;
-const int colorG = 0;
-const int colorB = 255;
+int colorR = 10;
+int colorG = 10;
+int colorB = 10;
+
+int tempchange = 0;
 
 const int B = 4275;               // B value of the thermistor
 const int R0 = 100000;            // R0 = 100k
@@ -15,23 +25,33 @@ void setup()
 {
     lcd.begin(16, 2);
     lcd.setRGB(colorR, colorG, colorB);
+    pinMode(ROTARY_ANGLE_SENSOR, INPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
+    
     int a = analogRead(pinTempSensor);
+    if(a != tempchange) 
+    {
+      tempchange = a;
+      float R = 1023.0/a-1.0;
+      R = R0*R;
+      float temp = 1.0/(log(R/R0)/B+1/298.15)-273.15;
+      temp = temp - 1.35;
+      Serial.println(temp);
+      lcd.setCursor(0, 0);
+      lcd.print("Temperature:");
+      lcd.setCursor(0, 1);
+      lcd.print(temp, 1);
+      delay(5000);
+    }
+    
 
-    float R = 1023.0/a-1.0;
-    R = R0*R;
- 
-    float temp = 1.0/(log(R/R0)/B+1/298.15)-273.15;
-    temp = temp - 1.35;
-    Serial.println(temp);
-    lcd.setCursor(0, 0);
-    lcd.print("Temperature:");
-    lcd.setCursor(0, 1);
-    lcd.print(temp, 1);
-    delay(1000);
+
+
+    
+    
     
 }
