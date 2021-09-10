022d12d52cc3f5a5b35df3a7aa3e406d237c13e4
CinisSec <gregorypro@protonmail.com>
2021-09-10 22:18:13 +0200

    RF send/receive testing
    inconclusive.

--- RF-send/RF-send.ino
+++ RF-send/RF-send.ino
@@ -12,33 +12,18 @@ const int colorB = 255;
 void setup()
 {
   vw_set_tx_pin(RF_TX_PIN); // Setup transmit pin
-  vw_setup(250); // Transmission speed in bits per second.
-  pinMode(3, INPUT);
+  vw_setup(2000); // Transmission speed in bits per second.
+  pinMode(6, INPUT);
   lcd.begin(16, 2);
   lcd.setRGB(colorR, colorG, colorB);
 }
  
 void loop()
 {
-  int buttonstate = digitalRead(3);
+  int buttonstate = digitalRead(6);
   const char *msg = "LDBP";
   if (buttonstate == HIGH){
     vw_send((uint8_t *)msg, strlen(msg));
-    lcd.clear();
-    lcd.setCursor(0,0); //set cursor on first line first char
-    lcd.setRGB(255,0,0);
-    lcd.print("Sending: ");
-    lcd.setCursor(0,1); //set cursor second line first char
-    lcd.print(msg);
-    delay(5000);
-  } else {
-    lcd.clear();
-    lcd.setRGB(colorR, colorG, colorB);
-    lcd.setCursor(0,0);
-    lcd.print("Wting for press.");
-    lcd.setCursor(0,1);
-    lcd.print("Press 1 sec");
-    delay(1000);
   }
  
 }
